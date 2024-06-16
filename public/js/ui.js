031fbb316d77cd1836a8e82bb0336770050df922
j8acob <jacobjfrench1@gmail.com>
2024-06-16 20:35:55 +1000

    display contacts
    Can now fully display contacts when they are clicked on.
    
    When a new contact is created that contact is set to the active contact to display.
    
    The user can now toggle between contacts, changing the active contact which is displayed and add new contacts as they please.

--- public/js/ui.js
+++ public/js/ui.js
@@ -54,7 +54,6 @@ alphabetSearch.initiate();
 const contentPanel = {
     state: null,
     setState: function(newState){
-        console.log("changing state");
         switch(this.state){
             case "newItemForm":
                 document.getElementById("add_icon").style.fill = "var(--color-text)";
@@ -65,6 +64,7 @@ const contentPanel = {
         if(this.state != "newItemForm"){
             load_content("html/newItemForm.html", "content", this.newItemFormLoaded);
             document.getElementById("add_icon").style.fill = "var(--color-active)";
+            navPanel.deselectItem();
             this.setState("newItemForm");
         }
     },
@@ -89,18 +89,98 @@ const contentPanel = {
 
         addressBook.addContact(contact);
         navPanel.populateItems();
+        navPanel.selectItem(contact.id);
     },
     displayItem: function(itemId){
-        load_content("html/displayItem.html", "content", this.itemDisplayed);
+        load_content("html/displayItem.html", "content", () => {this.itemDisplayed(itemId)});
         this.setState("displayItem");
     },
-    itemDisplayed: function(){
+    itemDisplayed: function(itemId){
+        let contact = addressBook.contacts.get(itemId);
+        let name = contact.firstName
+        if(contact.lastName != ""){
+            name = name + " " + contact.lastName;
+        }
 
+        document.getElementById("contactName").innerHTML = name;
+
+        let table = document.getElementById("displayItemTable");
+        let tr, td, text;
+        
+        //construct email section
+        if(contact.email != ""){
+            tr = document.createElement("tr");
+            th = document.createElement("th");
+            td = document.createElement("td");
+            text = document.createTextNode(contact.email);
+            td.appendChild(text);
+            text = document.createTextNode("Email:");
+            th.appendChild(text);
+            td.classList.add("text_light_dark");
+            th.classList.add("text_medium_dark");
+            tr.appendChild(th);
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
+
+        //construct mobile section
+        if(contact.mobile != ""){
+            tr = document.createElement("tr");
+            th = document.createElement("th");
+            td = document.createElement("td");
+            text = document.createTextNode(contact.mobile);
+            td.appendChild(text);
+            text = document.createTextNode("Mobile:");
+            th.appendChild(text);
+            td.classList.add("text_light_dark");
+            th.classList.add("text_medium_dark");
+            tr.appendChild(th);
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
+
+        //construct address section
+        if(contact.address != ""){
+            tr = document.createElement("tr");
+            th = document.createElement("th");
+            td = document.createElement("td");
+            text = document.createTextNode(contact.address);
+            td.appendChild(text);
+            text = document.createTextNode("Address:");
+            th.appendChild(text);
+            td.classList.add("text_light_dark");
+            th.classList.add("text_medium_dark");
+            tr.appendChild(th);
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
+
+        //construct notes section
+        if(contact.notes != ""){
+            tr = document.createElement("tr");
+            th = document.createElement("th");
+            th.classList.add("text_medium_dark");
+            text = document.createTextNode("Notes:");
+            th.appendChild(text);
+            tr.appendChild(th);
+            table.appendChild(tr);
+    
+            tr = document.createElement("tr");
+            tr.style.height = "0";
+            td = document.createElement("td");
+            let tdText = document.createTextNode(contact.notes);
+            td.appendChild(tdText);
+            td.colSpan = "2";
+            td.classList.add("text_light_dark");
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
     }
 }
 
 const navPanel = {
     nav: document.getElementById("navPanel"),
+    activeItem: null,
     populateItems: function(){
         this.nav.innerHTML = "";
         addressBook.contacts.forEach((value, key) => {
@@ -110,6 +190,7 @@ const navPanel = {
     constructItem: function(firstName, lastName, id){
         let container = document.createElement("div");
         container.classList.add("navItem");
+        container.id = "nav_item_" + id;
         container.onclick = () => {this.selectItem(id)};
         let title = document.createElement("h2");
         let text = document.createTextNode(firstName + " " + lastName);
@@ -118,7 +199,23 @@ const navPanel = {
         return container;
     },
     selectItem: function(id){
+        if(this.activeItem != null){
+            let activeItem = document.getElementById("nav_item_" + this.activeItem);
+            activeItem.classList.remove("selectedItem");
+            this.activeItem = null;
+        }
+        
+        this.activeItem = id;
+        let item = document.getElementById("nav_item_" + id);
+        item.classList.add("selectedItem");
         contentPanel.displayItem(id);
+    },
+    deselectItem: function(){
+        if(this.activeItem != null){
+            let activeItem = document.getElementById("nav_item_" + this.activeItem);
+            activeItem.classList.remove("selectedItem");
+            this.activeItem = null;
+        }
     }
 }
 
